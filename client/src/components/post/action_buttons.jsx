baaf920f24b00e70271df3f47bbfe9e1113f5e67
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2024-05-02 14:57:51 +0545

    like button

--- client/src/components/post/action_buttons.jsx
+++ client/src/components/post/action_buttons.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ActionButtons = () => {
+  const [liked, setLiked] = useState(false);
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className="flex justify-between items-center p-4">
       {/* love button */}
       <div className="flex flex-col justify-center items-center">
         <p>200K</p>
-        <button className="flex flex-col items-center space-x-2 text-gray-500">
-          <i class="fa-regular fa-heart"></i>
+        <button
+          className="flex flex-col items-center space-x-2 text-gray-500"
+          onClick={toggleLike}
+        >
+          {liked ? (
+            <i class="fa-solid fa-regular fa-heart" style={{color: "#63E6BE"}}></i>
+          ) : (
+            <i class="fa-regular fa-heart"></i>
+          )}
           Love
         </button>
       </div>
