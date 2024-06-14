0d66d05ee2e26f24399eb5ab328c8e8bf09af29a
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2024-06-14 19:26:29 +0545

    groups home add

--- client/src/components/groups/group_list.jsx
+++ client/src/components/groups/group_list.jsx
@@ -2,19 +2,16 @@ import React from "react";
 
 const GroupList = ({ items }) => {
   return (
-    <div className="overflow-y-auto max-h-40">
-      <ul className=" divide-y divide-gray-200">
+    <div className="overflow-y-auto max-h-80">
+      <ul className="p-2 divide-y divide-gray-200">
         {items.map((item, index) => (
-           <li key={index} className="py-2 flex justify-between">
-              <img src="" alt="Group's Photo" />
-              <div>
-                 
-              <div>
-              {item}
-              </div>
+          <li key={index} className="py-2 flex justify-between">
+            <img src="" alt="Group's Photo" />
+            <div>
+              <div>{item}</div>
               <div>Description about the Group</div>
-              </div>
-              <div className="flex">41K members</div>
+            </div>
+            <div className="flex">41K members</div>
           </li>
         ))}
       </ul>
--- client/src/pages/groups/groups_home.jsx
+++ client/src/pages/groups/groups_home.jsx
@@ -7,7 +7,8 @@ const GroupsHome = () => {
     <>
         <SearchBar placeholder={"Search for Groups"} />
         <div className="p-4">Sort Groups <button className="p-2 border shadow-md rounded-lg">sort</button> </div>
-      <GroupTabs />
+        <GroupTabs />
+        <button className="p-2 mt-8 ml-4 border shadow-md">Create Group</button>
     </>
   );
 };
@@ -16,7 +17,7 @@ const GroupTabs = () => {
   const [activeTab, setActiveTab] = useState(0);
 
   const tabs = [
-    { id: 0, name: "Joined Groups", items: ["Group 1", "Group 2", "Group 3"] },
+    { id: 0, name: "Joined Groups", items: ["Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6", "Group 7", "Group 8", "Group 9", "Group 10"] },
     { id: 1, name: "Popular Groups", items: ["Group 4", "Group 5", "Group 6"] },
     { id: 2, name: "Groups you Created", items: ["Group 7", "Group 8", "Group 9"] },
   ];
