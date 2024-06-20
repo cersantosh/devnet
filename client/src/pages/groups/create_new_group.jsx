69e4817834161bd236789773aa57abdcf7e380fd
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2024-06-20 14:58:51 +0545

    remove multiscreen group creation

--- client/src/pages/groups/create_new_group.jsx
+++ client/src/pages/groups/create_new_group.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ImageInput from "../../components/image_input";
 
 const CreateNewGroupModal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
   useEffect(() => {
     if (isOpen) {
       // Disable background scrolling when modal is open
@@ -16,11 +17,23 @@ const CreateNewGroupModal = ({ isOpen, onClose }) => {
       document.body.classList.remove("overflow-hidden");
     };
   }, [isOpen]);
-  if (!isOpen) return null;
+  const [groupName, setGroupName] = useState("");
+  const [groupDescription, setGroupDescription] = useState("");
+  const [groupPhoto, setGroupPhoto] = useState(null);
 
+  const handlePhotoChange = (e) => {
+    const file = e.target.files[0];
+    setGroupPhoto(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onClose();
+  };
   return (
     <>
-      <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="fixed z-10 inset-0">
         <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
             <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -32,17 +45,15 @@ const CreateNewGroupModal = ({ isOpen, onClose }) => {
           >
             &#8203;
           </span>
-          <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-            <header className="font-bold text-xl text-center mt-2">
-              Create a New Group
-            </header>
+          {/* div for the model */}
+          <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-y-auto h-full max-h-screen  shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               {/* div for close button */}
               <div className="absolute top-0 right-0 pt-2 pr-2">
                 <button
                   onClick={onClose}
                   type="button"
-                  className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150"
+                  className="text-gray-400 hover:text-gray-600 focus:outline-none focus:text-gray-600 transition ease-in-out duration-150"
                 >
                   <span className="sr-only">Close</span>
                   <svg
@@ -61,34 +72,46 @@ const CreateNewGroupModal = ({ isOpen, onClose }) => {
                   </svg>
                 </button>
               </div>
-              <form action="" method="post">
-                <div className="flex flex-col">
-                  <h1 className="font-semibold">Cover Photo:</h1>
-                  <ImageInput placeholder={"Select Cover Photo for Group"} />
-                  <label htmlFor="groupname" className="mt-2 font-semibold">
-                    Group Name:
-                  </label>
-                  <input
-                    className="block border-2 border-gray-300 rounded-lg p-2"
-                    type="text"
-                    name="groupname"
-                    id="groupname"
-                    placeholder="Give a descriptive name for your group"
-                  />
-                  <p className="font-semibold mt-2">Group Photo:</p>
-                  <ImageInput placeholder={"Select Photo for Group"} />
-                  <label htmlFor="groupname" className="mt-2 font-semibold">
-                    Description:
-                  </label>
-                  <input
-                    className="h-64 block border-2 border-gray-300 rounded-lg p-4"
-                    type="text"
-                    name="groupname"
-                    id="groupname"
-                    placeholder="Give a short description about your group"
-                  />
-                       </div>
-                     <input type="submit" value="Create Group" className="border-2" />  
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <header className="font-bold text-xl text-center mt-2">
+                    Create a New Group
+                  </header>
+                  <div className="flex flex-col">
+                    <h1 className="font-semibold">Cover Photo:</h1>
+                    <ImageInput placeholder={"Select Cover Photo for Group"} />
+                    <label htmlFor="groupname" className="mt-2 font-semibold">
+                      Group Name:
+                    </label>
+                    <input
+                      className="block border-2 border-gray-300 rounded-lg p-2"
+                      type="text"
+                      name="groupname"
+                      id="groupname"
+                      value={groupName}
+                      onChange={(e) => setGroupName(e.target.value)}
+                      placeholder="Give a descriptive name for your group"
+                    />
+
+                    <p className="font-semibold mt-2">Group Photo:</p>
+                    <ImageInput placeholder={"Select Photo for Group"} />
+                    <label
+                      htmlFor="groupDescription"
+                      className="mt-2 font-semibold"
+                    >
+                      Description:
+                    </label>
+                    <textarea
+                      id="groupDescription"
+                      name="groupDescription"
+                      className="block w-full border-2 border-gray-300 rounded-md mt-2"
+                      rows="9"
+                      placeholder="Give a short description about your group"
+                      value={groupDescription}
+                      onChange={(e) => setGroupDescription(e.target.value)}
+                    ></textarea>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
