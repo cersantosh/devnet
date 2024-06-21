8ff3b56aa7c79c5e788cd6fdbb3198ae682dc17c
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2024-06-21 15:31:41 +0545

    tweaking group model

--- client/src/pages/groups/create_new_group.jsx
+++ client/src/pages/groups/create_new_group.jsx
@@ -51,7 +51,7 @@ const CreateNewGroupModal = ({ isOpen, onClose }) => {
   };
   return (
     <>
-      <div className="fixed z-10 inset-0">
+      <div className="fixed overflow-y-auto z-10 inset-0">
         <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           {/* div for gray background */}
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
@@ -65,10 +65,10 @@ const CreateNewGroupModal = ({ isOpen, onClose }) => {
             &#8203;
           </span>
           {/* div for the model */}
-          <div className="inline-block align-bottom bg-white rounded-lg text-left h-full max-h-3/4 shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="inline-block align-bottom bg-white rounded-lg text-left h-full max-h-screen shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               {/* div for close button */}
-              <div className="sticky top-0 right-0 pt-2 pr-2">
+              <div className="fixed top-0 right-0 pt-2 pr-2">
                 <button
                   onClick={onClose}
                   type="button"
@@ -92,7 +92,7 @@ const CreateNewGroupModal = ({ isOpen, onClose }) => {
                 </button>
               </div>
               <form onSubmit={handleSubmit}>
-                <div className="overflow-y-auto">
+                <div>
                   <header className="sticky font-bold text-xl text-center mt-2">
                     Create a New Group
                   </header>
